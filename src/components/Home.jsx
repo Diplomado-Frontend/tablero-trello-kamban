e123c7ab625eb966df106871922bdc1ad2ef74d4
juanayala1 <juan.ayala@condorlabs.io>
2022-07-07 10:57:06 -0500

    Test into home the css class generatec from sass

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,22 @@
 
 import React, { useState } from "react";
-import LinkGitHubComp from "./BaseComp";
+import "../styles/sass/01_page/_container.scss";
 
 const HomeComp = (props) => {
 
    return (
       <>
-         <section>
-               <p>Hola XD</p>
-          </section>
+         <section className="container-trello">
+         <div className="todo item">
+            #TODO
+        </div>
+        <div className="inProgress item">
+            #IN PROGRESS
+        </div>
+        <div className="done item">
+            #DONE
+        </div>
+        </section>
       </>
     );
   }
