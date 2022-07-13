4cb2a098b713988774206413821802029c6d189f
juanayala1 <juan.ayala@condorlabs.io>
2022-07-12 19:21:55 -0500

    Create component to card list

---
+++ src/components/ListCard.jsx
@@ -0,0 +1,18 @@
+ 
+import React from "react";
+import "../styles/sass/01_page/_listCard.scss";
+
+const ListCard = (props) => 
+{
+    // const { githubLink } = props;
+
+    return (
+         <>
+            <div className="list-card">
+              #list card
+            </div>
+        </>  
+    )
+}
+
+export default ListCard;
