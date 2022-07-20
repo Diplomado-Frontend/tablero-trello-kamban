b28770d082f749a616102763c411aef0fa722175
IngJuanAyala <juanayalasalazar1@gmail.com>
2022-07-19 19:43:36 -0500

    Import generic component modal and set his events and data.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
  
-import React from "react";
+import React, { useState } from "react";
 import "../styles/sass/01_page/_card.scss";
+import Dialog from './generics/Dialog';
 
 const Card = ({ item }) => 
 {
@@ -8,11 +9,25 @@ const Card = ({ item }) =>
     item.type === "file" && (backgroundColor = "#f4d6d2");
     item.type === "img" && (backgroundColor = "#d9f3e9");
 
+    const [ openCardModal, setOpenCardModal ] = useState(false);
+
+    const handleClickOpenModal = () => {
+      setOpenCardModal(true);
+    }
+
     return (
          <>
-           <div className="card-item" style={Object.assign({}, { backgroundColor })}>
+           <div className="card-item" style={Object.assign({}, { backgroundColor })} onClick={handleClickOpenModal}>
             <h4>{item.name}</h4>
           </div>
+
+          { openCardModal && (
+              <Dialog
+                title={item.name}
+                description={item.description}
+                open={openCardModal}
+          />
+      )}
           
         </>  
     )
